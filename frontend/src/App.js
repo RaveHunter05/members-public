e7d68f56f794139d1d0f00687dce45f522a750e8
Paul Sotelo <francisco.sotelo@meltstudio.co>
2022-06-26 07:58:11 -0600

    fix: some css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import { Auth, Members } from "./components";
 import RequiredAuth from "./routes/RequiredAuth";
 
+import "./App.css";
+
 import styled from "styled-components";
 
 const MainContainer = styled.div`
   height: 100vh;
   width: 100vw;
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
